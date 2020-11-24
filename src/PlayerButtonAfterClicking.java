bcd092ca82e7ae5f9f93faece6be5c8044cc16e0
defnebciftci <45923713+defnebciftci@users.noreply.github.com>
2020-11-25 01:48:17 +0300

    Delete PlayerButtonAfterClicking.java

--- src/PlayerButtonAfterClicking.java
+++
@@ -1,8 +0,0 @@
-import javafx.scene.control.Button;
-
-public class PlayerButtonAfterClicking extends PlayerButton {
-    //get player as a parameter
-    PlayerButtonAfterClicking( double locX, double locY) {
-        super( locX, locY);
-    }
-}
