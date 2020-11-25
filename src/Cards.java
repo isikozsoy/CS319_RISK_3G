7127d111de2924b6a2b92bc28e9fc202c40c969e
Alp Uneri <alpuneri1560@gmail.com>
2020-11-25 23:28:48 +0300

    Wrote Card and Cards class.

---
+++ src/Cards.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Cards {
+    ArrayList<Card> cards;
+
+    //creates a set of cards with the specified number of infantry, cavalry, cannon and joker cards
+    public Cards(int noOfInfantry, int noOfCavalry, int noOfCannon, int noOfJoker) {
+        //create the arraylist
+        cards = new ArrayList<Card>();
+
+        //add appropriate number of infantry cards
+        for (int i = 0; i < noOfInfantry; i++) {
+            Card infantry = new Card(cardType.INFANTRY);
+            cards.add(infantry);
+        }
+
+        //add appropriate number of cavalry cards
+        for (int i = 0; i < noOfCavalry; i++) {
+            Card cavalry = new Card(cardType.CAVALRY);
+            cards.add(cavalry);
+        }
+
+        //add appropriate number of cannon cards
+        for (int i = 0; i < noOfCannon; i++) {
+            Card cannon = new Card(cardType.CANNON);
+            cards.add(cannon);
+        }
+
+        //add appropriate number of joker cards
+        for (int i = 0; i < noOfJoker; i++) {
+            Card joker = new Card(cardType.JOKER);
+            cards.add(joker);
+        }
+
+        shuffle();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        Card card = cards.get(0);
+        cards.remove(0);
+        return card;
+    }
+
+    public void addCard(cardType type) {
+        Card toBeAdded = new Card(type);
+        cards.add(toBeAdded);
+        shuffle();
+    }
+}
