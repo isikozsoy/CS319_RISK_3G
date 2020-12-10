c0376b636bb326f2e40d3d2244fc71ea1ddd31af
burakyetistiren <burakyetistiren@hotmail.com>
2020-12-10 21:26:18 +0300

    continent class to master branch

--- src/Continent.java
+++ src/Continent.java
@@ -6,6 +6,7 @@ public class Continent {
     private ArrayList<Territory> territories;
     private int bonusTroopCount;
 
+
     //constructor
     Continent(String name, int bonusTroopCount, ArrayList<Territory> territories)
     {
