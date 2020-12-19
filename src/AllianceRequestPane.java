894e24c97d90bf2d7e665ddfc57f283d7d9a7875
burakyetistiren <burakyetistiren@hotmail.com>
2020-12-19 22:26:28 +0300

    UI improved

--- src/AllianceRequestPane.java
+++ src/AllianceRequestPane.java
@@ -1,4 +1,5 @@
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -14,10 +15,18 @@ public class AllianceRequestPane extends VBox {
     private int requestCount;
     HashMap<Integer, Integer> acceptedRequests;
     ArrayList<AllianceRequest> requestsElements;
+    private Player player;
 
+    final String styleBackground = "-fx-background-color: #ff6666;" +
+            "-fx-border-color: #00ccff";
+    final String styleBackgroundButton = "-fx-background-color: #ff6666;";
 
     public AllianceRequestPane() {
-        // Empty constructor
+        Label requests = new Label("Alliance requests");
+        requests.setFont(Font.font("SNAP ITC", 20));
+        this.getChildren().add(requests);
+        requests.setStyle(styleBackground);
+        requests.setMaxSize(350,75);
         requestCount = 0;
         requestsElements = new ArrayList<>();
     }
@@ -44,6 +53,7 @@ public class AllianceRequestPane extends VBox {
     }
 
     public void setAllianceRequests(Player curPlayer) {
+        player = curPlayer;
         requestCount = curPlayer.getAllianceReq().size();
         HashMap<Integer, String> requests = curPlayer.getAllianceReq();
         Iterator iterator = requests.entrySet().iterator();
@@ -59,14 +69,15 @@ public class AllianceRequestPane extends VBox {
 
     class AllianceRequest extends HBox {
         private int id;
-        private Text playerName;
+        private Label playerName;
         Button acceptButton;
         Button ignoreButton;
 
         AllianceRequest(int id, String name) {
             this.id = id;
-            playerName = new Text(name + "  ");
-            playerName.setFont(Font.font("SNAP ITC", 25));
+            playerName = new Label(name + "  ");
+            playerName.setFont(Font.font("SNAP ITC", 20));
+
             acceptButton = new Button("ACCEPT");
             ImageView acceptIcon = new ImageView(new Image("icons/accept_icon.png"));
             acceptIcon.setFitHeight(15);
@@ -79,6 +90,17 @@ public class AllianceRequestPane extends VBox {
             ignoreIcon.setFitWidth(15);
             ignoreButton.setGraphic(ignoreIcon);
 
+            acceptButton.setStyle(styleBackgroundButton);
+            ignoreButton.setStyle(styleBackgroundButton);
+
+            acceptButton.setFont(Font.font("SNAP ITC", 15));
+            ignoreButton.setFont(Font.font("SNAP ITC", 15));
+
+            acceptButton.setMaxSize(350,60);
+            ignoreButton.setMaxSize(350,60);
+
+            this.setStyle(styleBackground);
+            this.setMaxSize(350, 200);
             this.getChildren().addAll(playerName, acceptButton, ignoreButton);
         }
         public Button getIgnoreButton() {
