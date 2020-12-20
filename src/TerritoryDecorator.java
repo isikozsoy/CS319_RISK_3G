3a6e4ffbaa9813298ba0e869882cdd7c90a0a0f6
Mustafa Hakan Kara <kara@mustafahakan.com>
2020-12-20 16:27:17 +0300

    RPSView and TerritoryDecorater were added.

---
+++ src/TerritoryDecorator.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+
+public abstract class TerritoryDecorator extends Territory {
+
+    //property
+    protected Territory territory;
+
+    //constructor
+    public TerritoryDecorator(Territory territory) {
+        super(territory.name, territory.id);
+        this.territory = territory;
+    }
+
+    //methods
+    public Player getOwner() { return territory.owner; }
+
+    public void setOwner(Player owner) { territory.setOwner(owner); }
+
+    public int getTroopCount() {
+        return territory.getTroopCount();
+    }
+
+    public void setTroopCount(int troopCount) {
+        territory.setTroopCount(troopCount);
+    }
+
+    public boolean hasAirport() { return territory.hasAirport(); }
+
+    public void setHasAirport(boolean hasAirport) { territory.setHasAirport(hasAirport); }
+
+    public HashSet<Territory> getNeighbors() {
+        return territory.getNeighbors();
+    }
+
+    public void setNeighbors(HashSet<Territory> neighbors) {
+        territory.setNeighbors(neighbors);
+    }
+
+    // Adds given Territory parameter to the neighbors hashset
+    public void addNeighbor(Territory neighbor)
+    {
+        territory.addNeighbor(neighbor);
+    }
+
+    // Checks if a given Territory object
+    // is contained in the neighbors hashset
+    public boolean isNeighbor(Territory territory)
+    {
+        return neighbors.contains(territory);
+    }
+}
